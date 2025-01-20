9594d4b855f648f72286c3ea513cc20c406e1466
Nikita Bystritsky <nbystritsky@gmail.com>
2025-01-20 13:21:58 +0500

    feat: add solution to task two

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'output.txt');
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Enter text.');
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Bye!');
+    rl.close();
+    writeStream.end();
+    process.exit();
+  } else {
+    writeStream.write(input + '\n');
+  }
+});
+
+rl.on('SIGINT', () => {
+  console.log('Bye!');
+  rl.close();
+  writeStream.end();
+  process.exit();
+});
