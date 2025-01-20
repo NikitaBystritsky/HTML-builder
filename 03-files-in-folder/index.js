4dec9fccef8a76da255ab41e03397986a47f2e60
Nikita Bystritsky <nbystritsky@gmail.com>
2025-01-20 13:31:07 +0500

    feat: add solution to task three

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+async function displayFileInfo() {
+  try {
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const fileExtension = path.extname(file.name).slice(1);
+
+        const filePath = path.join(folderPath, file.name);
+        const stats = await fs.stat(filePath);
+
+        console.log(
+          `${file.name} - ${fileExtension} - ${stats.size / 1000} kb`,
+        );
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+displayFileInfo();
